bb6f5408783b0e4cfc10298f79733907e6c61832
Vishal <manevishal619@gmail.com>
2024-07-22 22:13:35 +0530

    Live API Data

--- app/consumer.py
+++ app/consumer.py
@@ -9,7 +9,7 @@ import logging
 logging.basicConfig(
     filename=f'{os.getenv("LOGS_DIR")}/consumer.log',
     level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
 logger = logging.getLogger(__name__)
@@ -21,14 +21,16 @@ credentials_location = os.path.join(root_dir, "config", "credential.json")
 
 try:
     with open(credentials_location) as f:
-            credentials = json.load(f)
+        credentials = json.load(f)
 except FileNotFoundError:
-    raise FileNotFoundError(f"Credentials File Not Found At Location {credentials_location}")
+    raise FileNotFoundError(
+        f"Credentials File Not Found At Location {credentials_location}"
+    )
 
 
 BOOTSTRAP_SERVER = credentials["BOOTSTRAP_SERVER"]
 BOOTSTRAP_PORT = credentials["BOOTSTRAP_PORT"]
-CONSUMER_GROUP_ID = credentials["CONSUMER_GROUP_ID"] 
+CONSUMER_GROUP_ID = credentials["CONSUMER_GROUP_ID"]
 MYSQL_HOST = credentials["MYSQL_HOST"]
 MYSQL_PORT = credentials["MYSQL_PORT"]
 MYSQL_USER = credentials["MYSQL_USER"]
@@ -39,12 +41,13 @@ TOPIC = credentials["TOPIC"]
 
 # Kafka configuration
 conf = {
-    'bootstrap.servers': f'{BOOTSTRAP_SERVER}:{BOOTSTRAP_PORT}',
-      'group.id':  CONSUMER_GROUP_ID
+    "bootstrap.servers": f"{BOOTSTRAP_SERVER}:{BOOTSTRAP_PORT}",
+    "group.id": CONSUMER_GROUP_ID,
 }
 
 # Create Consumer instance
 consumer = Consumer(conf)
+
 
 def connect_to_mysql():
     """
@@ -56,7 +59,7 @@ def connect_to_mysql():
             port=MYSQL_PORT,
             user=MYSQL_USER,
             password=MYSQL_PASSWORD,
-            database=MYSQL_DATABASE
+            database=MYSQL_DATABASE,
         )
         return connection
     except pymysql.Error as err:
@@ -72,20 +75,41 @@ def insert_data_to_mysql(data):
     if connection:
         try:
             cursor = connection.cursor()
-            create_table = f'''
+            create_table = f"""
                             CREATE TABLE IF NOT EXISTS {MYSQL_TABLE}( 
-                                    id INT, 
-                                    value DECIMAL(20,20), 
-                                    timestamp INT 
+                                    company VARCHAR(20), 
+                                    timestamp INT,
+                                    open DECIMAL(15,4),
+                                    close DECIMAL(15,4),
+                                    high DECIMAL(15,4),
+                                    low DECIMAL(15,4),
+                                    previous_close DECIMAL(15,4),
+                                    volume BIGINT
                                 )
-                            '''
+                            """
             cursor.execute(create_table)
             connection.commit()
 
             # Prepare SQL statement based on your table schema
-            sql = f"INSERT INTO {MYSQL_TABLE} (id, value, timestamp) VALUES (%s, %s, %s)"
-            cursor.execute(sql, (data['id'], data['value'], data['timestamp']))
+            sql = f"""
+                INSERT INTO {MYSQL_TABLE} (company, timestamp, open, close, high, low, previous_close, volume)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+            """
+            cursor.execute(
+                sql,
+                (
+                    data["code"],
+                    data["timestamp"],
+                    data["open"],
+                    data["close"],
+                    data["high"],
+                    data["low"],
+                    data["previousClose"],
+                    data["volume"],
+                ),
+            )
             connection.commit()
+
             logger.info(f"Data inserted successfully!")
         except pymysql.Error as err:
             logger.error(f"Error inserting data: {err}")
@@ -105,10 +129,10 @@ def consume_and_process():
         elif msg.error():
             logger.error(f"Consumer error: {msg.error()}")
         else:
-            data = json.loads(msg.value().decode('utf-8'))
+            data = json.loads(msg.value().decode("utf-8"))
             insert_data_to_mysql(data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     consume_and_process()
     consumer.close()
--- app/producer.py
+++ app/producer.py
@@ -1,15 +1,14 @@
 from confluent_kafka import Producer
 import json
 import time
-import random
 import os
 import logging
-
+import requests
 
 logging.basicConfig(
     filename=f'{os.getenv("LOGS_DIR")}/producer.log',
     level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 
 logger = logging.getLogger(__name__)
@@ -21,51 +20,54 @@ credentials_location = os.path.join(root_dir, "config", "credential.json")
 
 try:
     with open(credentials_location) as f:
-            credentials = json.load(f)
+        credentials = json.load(f)
 except FileNotFoundError:
-    raise FileNotFoundError(f"Credentials File Not Found At Location {credentials_location}")
+    raise FileNotFoundError(
+        f"Credentials File Not Found At Location {credentials_location}"
+    )
 
 BOOTSTRAP_SERVER = credentials["BOOTSTRAP_SERVER"]
 BOOTSTRAP_PORT = credentials["BOOTSTRAP_PORT"]
 TOPIC = credentials["TOPIC"]
+API_TOKEN = credentials["API_TOKEN"]
 
 
 conf = {
-    'bootstrap.servers': f'{BOOTSTRAP_SERVER}:{BOOTSTRAP_PORT}',
+    "bootstrap.servers": f"{BOOTSTRAP_SERVER}:{BOOTSTRAP_PORT}",
 }
 
 
 # Create Producer instance
 producer = Producer(conf)
 
+
 def delivery_report(err, msg):
     """Delivery report callback called once for each produced message."""
     if err is not None:
         logger.error(f"Message delivery failed: {err}")
     else:
-        logger.info(f"Message delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+        logger.info(
+            f"Message delivered to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}"
+        )
 
-def generate_sample_data():
-    """Generate sample data to send to Kafka."""
-    return {
-        'id': random.randint(1, 1000),
-        'value': random.random(),
-        'timestamp': int(time.time())
-    }
 
 if __name__ == "__main__":
     # Produce messages
     try:
         while True:
-            message = generate_sample_data()
-            producer.produce(TOPIC, value=json.dumps(message), callback=delivery_report)
-            producer.poll(0)  # Poll to handle delivery reports and other events
-            time.sleep(1)  # Simulate delay between messages
+            url = f"https://eodhd.com/api/real-time/AAPL.US?s=VTI,EUR.FOREX&api_token={API_TOKEN}&fmt=json"
+            stocks = requests.get(url).json()
 
-        # Wait for any outstanding messages to be delivered and delivery reports to be received
-            producer.flush()
+            for stock in stocks:
+                print(stock)
+                producer.produce(
+                    TOPIC, value=json.dumps(stock), callback=delivery_report
+                )
+                producer.poll(0)
+                producer.flush()
+            time.sleep(60)
+
     except Exception as e:
         logger.exception(f"An error occurred: {e}")
     finally:
         producer.flush()  # Ensure all messages are sent before exiting
-
